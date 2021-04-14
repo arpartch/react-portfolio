24809a8b0b5c436aa7090e435f7110af91937773
arpartch <athena.partch@gmail.com>
2021-04-13 17:11:05 -0700

    2 files modified, 1 added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import UserHeader from './UserHeader'
+export default UserHeader
